708b3b4216275e3c87298adc010b8feae735a9af
spartakb <spartak@gmail.com>
2019-04-13 11:30:26 -0700

    Installed cocopods and added gitignore

---
+++ Pods/Target Support Files/Pods-CHLA Augmented Reality PrototypeTests/Pods-CHLA Augmented Reality PrototypeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CHLA_Augmented_Reality_PrototypeTests : NSObject
+@end
+@implementation PodsDummy_Pods_CHLA_Augmented_Reality_PrototypeTests
+@end
---
+++ Pods/Target Support Files/Pods-CHLA Augmented Reality PrototypeUITests/Pods-CHLA Augmented Reality PrototypeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CHLA_Augmented_Reality_PrototypeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CHLA_Augmented_Reality_PrototypeUITests
+@end
